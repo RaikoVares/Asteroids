937cda7cbe6a6d2f71f26917f0985ca81c11afaa
Vares <raiko_vares@hotmail.com>
2025-04-20 14:43:47 +0300

    Added player

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 
 
 from constants import *
+from player import Player
 
 
 def main():
@@ -16,6 +17,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player = Player(x = SCREEN_WIDTH / 2, 
+                        y = SCREEN_HEIGHT / 2)
+        player.draw(screen)
+
         pygame.display.flip()
         time_passed = clock.tick(60)
         dt = time_passed / 1000
