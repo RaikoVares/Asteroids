e8c8329d89a85344a868dba6045b023bf4b63556
Vares <raiko_vares@hotmail.com>
2025-04-20 15:26:07 +0300

    Added rotation

--- main.py
+++ main.py
@@ -12,16 +12,18 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    player = Player(x = SCREEN_WIDTH / 2, 
+                    y = SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("black")
-        player = Player(x = SCREEN_WIDTH / 2, 
-                        y = SCREEN_HEIGHT / 2)
-        player.draw(screen)
 
+        screen.fill("black")
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
+
         time_passed = clock.tick(60)
         dt = time_passed / 1000
     print("Starting Asteroids!")
